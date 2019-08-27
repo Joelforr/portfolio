4324220b7ae84ef80bf6f9a2642c4ea7107da4e4
DESKTOP\Joel <joelforr@gmail.com>
2019-08-27 03:02:32 -0400

    Updates

---
+++ precache-manifest.9ec4cb837f1633e8e3509e21c175d78a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "e15487f66e278609c1ce",
+    "url": "/static/css/main.63b5fc55.chunk.css"
+  },
+  {
+    "revision": "e15487f66e278609c1ce",
+    "url": "/static/js/main.e15487f6.chunk.js"
+  },
+  {
+    "revision": "12461019f42f6cac0319",
+    "url": "/static/css/1.b697cd24.chunk.css"
+  },
+  {
+    "revision": "12461019f42f6cac0319",
+    "url": "/static/js/1.12461019.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "242b3f742464e0b0c5238fc705098f61",
+    "url": "/static/media/Shoot_VR.242b3f74.jpg"
+  },
+  {
+    "revision": "e2da0b6cce8f84060d6a2465896f0c47",
+    "url": "/static/media/logo.e2da0b6c.svg"
+  },
+  {
+    "revision": "418e35647ac86b2bb75009e9d5cdcf27",
+    "url": "/static/media/resume.418e3564.pdf"
+  },
+  {
+    "revision": "15d32015acdeb7f1354a5869c8209bfb",
+    "url": "/static/media/continuum_1.15d32015.jpg"
+  },
+  {
+    "revision": "50b8bc5ddc2c4222d64efe5c6d8897d1",
+    "url": "/static/media/flight-club_1.50b8bc5d.png"
+  },
+  {
+    "revision": "bd7b92bf03cd0e5e88252ff7ae16b23b",
+    "url": "/static/media/flight-club_2.bd7b92bf.png"
+  },
+  {
+    "revision": "fb82871ed63f0ed37687b631aa6e1e7c",
+    "url": "/static/media/flight-club_3.fb82871e.png"
+  },
+  {
+    "revision": "1907a84642133dcf130f7283a83bd6f8",
+    "url": "/static/media/flight-club_4.1907a846.png"
+  },
+  {
+    "revision": "0b1f512844453661fff1c1ec98d3bf4f",
+    "url": "/static/media/logo.0b1f5128.png"
+  },
+  {
+    "revision": "683d10a98b0b23bec6dcd03ab2dd1aae",
+    "url": "/static/media/pulse_1.683d10a9.png"
+  },
+  {
+    "revision": "87c2d008052a68d25a839f12af53ab07",
+    "url": "/static/media/pulse_2.87c2d008.png"
+  },
+  {
+    "revision": "b53191d10d88436062fd34ed459b096e",
+    "url": "/static/media/pulse_3.b53191d1.png"
+  },
+  {
+    "revision": "3554da0bcc768a29ef2567447e66228d",
+    "url": "/static/media/pulse_Ico.3554da0b.png"
+  },
+  {
+    "revision": "81ac553cbf863118c90760102dd04b05",
+    "url": "/static/media/skull-gem_1.81ac553c.png"
+  },
+  {
+    "revision": "1364b138f7877e841b525c39851cf41c",
+    "url": "/index.html"
+  }
+];
